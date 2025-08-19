cc44261942586dcc1f510c3a8dd955a1706a65ef
Sergey Sterkhov <129599465+ElecSir@users.noreply.github.com>
2025-08-19 11:06:46 +0300

    Update 24.sql

--- SQL/Solutions/24.sql
+++ SQL/Solutions/24.sql
@@ -1,12 +1,11 @@
 -- Найдите страны, имевшие когда-либо классы обычных боевых кораблей ('bb') и имевшие когда-либо классы крейсеров ('bc').
 
-SELECT country FROM classes
-WHERE type = 'bc'
-AND country IN (
-              SELECT country FROM classes
-              WHERE type = 'bb'
-              GROUP BY country
-              HAVING COUNT(type) > 0
-)
-GROUP BY country
-HAVING COUNT(type) > 0
+SELECT country
+FROM Classes
+WHERE type = 'bb'
+
+INTERSECT
+
+SELECT country
+FROM Classes
+WHERE type = 'bc';
