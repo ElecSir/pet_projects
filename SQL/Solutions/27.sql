8c05b6f8f0a3c7b03fd40eb61df61453d8ebe2f8
Sergey Sterkhov <129599465+ElecSir@users.noreply.github.com>
2025-08-19 13:57:10 +0300

    Create 27.sql

---
+++ SQL/Solutions/27.sql
@@ -0,0 +1,7 @@
+-- Укажите сражения, которые произошли в годы, не совпадающие ни с одним из годов спуска кораблей на воду.
+
+SELECT name FROM battles
+WHERE YEAR(date) NOT IN (
+        SELECT launched FROM ships
+        WHERE launched IS NOT NULL
+)
